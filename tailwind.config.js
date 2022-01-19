6186a47e84a091a69255433b32ef2cca6ab7fc17
johnchowssc <71998398+johnchowssc@users.noreply.github.com>
2022-01-19 12:28:28 +1100

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./app/templates/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+    }
+  },
+  plugins: [],
+}
