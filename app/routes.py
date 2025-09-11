02561808c6f0c6a2b3f46d0279481ed50077bf69
John Bradstock Chow <john.chow@sydneysabre.com>
2025-09-11 17:25:54 +1000

    enable users to edit their own entry

--- app/routes.py
+++ app/routes.py
@@ -482,13 +482,13 @@ def edit_student_self(student_id):
             parent_id = student.parent_id,
             user_id = current_user.id
         )
-        student.name = new_student.name
-        student.email = new_student.email
-        student.mobile = new_student.mobile
-        student.paid = new_student.paid
-        student.parent_id = new_student.parent_id
+        #student.name = new_student.name
+        #student.email = new_student.email
+        #student.mobile = new_student.mobile
+        #student.paid = new_student.paid
+        #student.parent_id = new_student.parent_id
         #student.user_id = new_student.user_id
-        #db.session.commit()
+        db.session.commit()
         return redirect(url_for('book_class', class_slot_id=student.parent_id))
     ## Prepopulate values in form
     form.name.data = student.name
